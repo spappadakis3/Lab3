c1ae19fd09f147df8045ffc92a85ed619f353393
spappadakis3 <spappadakis3@gmail.com>
2025-04-15 23:00:42 -0400

    ParadiseBot.py

--- pages/ParadiseBot.py
+++ pages/ParadiseBot.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 st.write("This is phase 4 chatbot implementation")
 st.write("The chatbot can answer questions about what happened in certain episodes or facts about the actors")
 st.write("these are just ideas for how we could use it we can do other stuff too")
