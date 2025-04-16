406394f602adb4a1116cbe1541d10b72f9f0dda6
spappadakis3 <spappadakis3@gmail.com>
2025-04-15 22:46:25 -0400

    ParadiseBot.py

--- pages/ParadiseBot.py
+++ pages/ParadiseBot.py
@@ -3,5 +3,8 @@ st.write("This is phase 4 chatbot implementation")
 st.write("The chatbot can answer questions about what happened in certain episodes or facts about the actors")
 st.write("these are just ideas for how we could use it we can do other stuff too")
 
-chatPrompt = st.chat_input("Type prompt here") #Creates a chat input field in which a user can type in a prompt
+chatPrompt = st.chat_input("Type prompt here")
+st.write(chatPrompt)
 
+with st.chat_message("user"):
+    st.write("Hello!")
