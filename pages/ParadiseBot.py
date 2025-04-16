fdafc6c2b20cea87b5037537151f42f5e2607462
spappadakis3 <spappadakis3@gmail.com>
2025-04-15 22:58:06 -0400

    ParadiseBot.py

--- pages/ParadiseBot.py
+++ pages/ParadiseBot.py
@@ -3,11 +3,29 @@ st.write("This is phase 4 chatbot implementation")
 st.write("The chatbot can answer questions about what happened in certain episodes or facts about the actors")
 st.write("these are just ideas for how we could use it we can do other stuff too")
 
-with st.chat_message("user"):
-    st.write("Hello! What can I help you with?")
 
+def response_generator():
+    response = random.choice(["Hi there! What can I help you with?","Hi, human! What's up?","Can I help you?"])
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.03)
+        
 chatPrompt = st.chat_input("Type prompt here")
-st.write(chatPrompt)
 
-if chatPrompt == 'Hi':
-    st.write("Hi There!")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt == st.chat_input("Type prompt here"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+with st.chat_message("assistant"):
+    response = st.write(response_generator())
+
