a3a534ef2eed2b2a37c644c2b5b5e119c163606d
spappadakis3 <spappadakis3@gmail.com>
2025-04-16 00:02:24 -0400

    ParadiseBot.py

--- pages/ParadiseBot.py
+++ pages/ParadiseBot.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import random
 import time
+import Paradise_LLM_Analysis
+
+
+
 st.write("This is phase 4 chatbot implementation")
 st.write("The chatbot can answer questions about what happened in certain episodes or facts about the actors")
 st.write("these are just ideas for how we could use it we can do other stuff too")
 
+key = st.secrets['key']
 
 
 def response_generator(prompt):
@@ -26,9 +31,13 @@ if prompt := st.chat_input("What can I help you with?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-        
-    with st.chat_message("assistant"):
-        response = st.write_stream(response_generator(prompt))
+
+     if key:
+          llm_response = Paradise_LLM_Analysis.generate_llm_response(prompt, key)
+          st.session_state.messages.append({"role": "assistant", "content": llm_response})
+          with st.chat_message("assistant"):
+            st.markdown(llm_response)
+
         
     st.session_state.messages.append({"role": "assistant", "content": response})
 
