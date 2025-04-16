39b70b5caf4257b369d4b57376aab9f89d826e25
spappadakis3 <spappadakis3@gmail.com>
2025-04-15 23:28:27 -0400

    ParadiseBot.py

--- pages/ParadiseBot.py
+++ pages/ParadiseBot.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import random
+import time
 from openai import OpenAI
 from google.generativeai as genai
 st.write("This is phase 4 chatbot implementation")
 st.write("The chatbot can answer questions about what happened in certain episodes or facts about the actors")
 st.write("these are just ideas for how we could use it we can do other stuff too")
 
+'''
 api_key = "AIzaSyAePJnDvz8j0N-1kp7_2lxw1q8r9V6ZMsQ"
 #api_key = st.secrets["api_key"]
 client = OpenAI(api_key=api_key)
@@ -21,12 +23,14 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+'''
 
-
-def response_generator():
-    response = random.choice(["Hi there! What can I help you with?","Hi, human! What's up?","Can I help you?"])
+def response_generator(prompt):
+    #response = random.choice(["Hi there! What can I help you with?","Hi, human! What's up?","Can I help you?"])
+    response = prompt
     for word in response.split():
         yield word + " "
+        time.sleep(.03)
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -44,8 +48,7 @@ if prompt := st.chat_input("Type prompt here"):
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(model=st.session_state["openai_model"],messages=[{"role": m["role"], "content": m["content"]}for m in st.session_state.messages],stream=True,)
-        response = st.write_stream(stream)
+        response = st.write(response_generator(prompt))
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 
