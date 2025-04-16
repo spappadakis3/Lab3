2ad7e6b09cb61aeeb4068945ac6475c1a25885ae
spappadakis3 <spappadakis3@gmail.com>
2025-04-16 13:12:35 -0400

    ParadiseBot.py

--- pages/ParadiseBot.py
+++ pages/ParadiseBot.py
@@ -70,6 +70,7 @@ if prompt := st.chat_input("Ask me anything about the show!"):
             api_info = getEpisodes()
             inRange = False
             for i in range(10):
+                st.markdown("is running loop")
                 if i in prompt:
                     inRange = True
                     api_info = specificEpisode(i)
