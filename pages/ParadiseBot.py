9829801a6b48d4b1d2b25daff7e7c0a245831cc2
spappadakis3 <spappadakis3@gmail.com>
2025-04-15 23:29:06 -0400

    ParadiseBot.py

--- pages/ParadiseBot.py
+++ pages/ParadiseBot.py
@@ -7,23 +7,6 @@ st.write("This is phase 4 chatbot implementation")
 st.write("The chatbot can answer questions about what happened in certain episodes or facts about the actors")
 st.write("these are just ideas for how we could use it we can do other stuff too")
 
-'''
-api_key = "AIzaSyAePJnDvz8j0N-1kp7_2lxw1q8r9V6ZMsQ"
-#api_key = st.secrets["api_key"]
-client = OpenAI(api_key=api_key)
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gemini-2.0-flash"
-
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-'''
 
 def response_generator(prompt):
     #response = random.choice(["Hi there! What can I help you with?","Hi, human! What's up?","Can I help you?"])
