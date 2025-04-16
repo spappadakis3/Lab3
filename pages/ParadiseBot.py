7fd6a34a5c1bf585382ce72d8515a247a039b116
spappadakis3 <spappadakis3@gmail.com>
2025-04-15 22:47:44 -0400

    ParadiseBot.py

--- pages/ParadiseBot.py
+++ pages/ParadiseBot.py
@@ -3,8 +3,11 @@ st.write("This is phase 4 chatbot implementation")
 st.write("The chatbot can answer questions about what happened in certain episodes or facts about the actors")
 st.write("these are just ideas for how we could use it we can do other stuff too")
 
+with st.chat_message("user"):
+    st.write("Hello! What can I help you with?")
+
 chatPrompt = st.chat_input("Type prompt here")
 st.write(chatPrompt)
 
-with st.chat_message("user"):
-    st.write("Hello!")
+if chatPrompt == 'Hi':
+    st.write("Hi There!")
