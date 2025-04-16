fdb953a776f44370f01c54cc515f9cb5dd034862
spappadakis3 <spappadakis3@gmail.com>
2025-04-16 08:49:43 -0400

    ParadiseBot.py

--- pages/ParadiseBot.py
+++ pages/ParadiseBot.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import random
 import time
-import pages/Paradise_LLM_Analysis as Paradise_LLM_Analysis
-
+import google.generativeai as genai
 
 
 st.write("This is phase 4 chatbot implementation")
@@ -10,34 +9,46 @@ st.write("The chatbot can answer questions about what happened in certain episod
 st.write("these are just ideas for how we could use it we can do other stuff too")
 
 key = st.secrets['key']
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('gemini-2.0-flash')
 
+def getEpisodes():
+    url = f"https://api.tvmaze.com/shows/75030/episodes"
+    response = requests.get(url)
+    episodes = response.json()
 
-def response_generator(prompt):
-    #response = random.choice(["Hello there! How can I assist you today?","Hi, human! Is there anything I can help you with?","Do you need help?"])
-    response = prompt
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
+    episodeList = []
+    for ep in episodes:
+        episodeList.append(ep)
+    return episodeList
+
         
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
     
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What can I help you with?"):
+if prompt := st.chat_input("Ask me anything about the show!"):
 
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
+    try:
+        if "episode" in prompt.lower():
+            api_info = get_show_episodes()
+            reply = f"Here are some episodes I found:\n\n{api_info}"
+        else:
+            gemini_response = model.generate_content(prompt)
+            reply = gemini_response.text
 
-     if key:
-          llm_response = Paradise_LLM_Analysis.generate_llm_response(prompt, key)
-          st.session_state.messages.append({"role": "assistant", "content": llm_response})
-          with st.chat_message("assistant"):
-            st.markdown(llm_response)
+        st.session_state.messages.append({"role": "assistant", "content": reply})
+        with st.chat_message("assistant"):
+            st.markdown(reply)
+    except:
+        st.chat_message("assistant")
+        st.error("Error")
 
-        
-    st.session_state.messages.append({"role": "assistant", "content": response})
 
